38b9fb3c780d9d861da7da8a81582d21303d8d18
sainsh <kfsainsh@gmail.com>
2020-04-20 15:19:39 +0200

    updated buttons updated text navigation now works rasp_paste outcommented

--- gui.py
+++ gui.py
@@ -1,13 +1,16 @@
 import pygame
 import sys
 import time
-from rasp_paste import goUp
-from rasp_paste import goDown
+from pygame.locals import *
+#import rasp_paste as rp
 
 width = 800
 height = 600
 size = [width, height]
 bg = [0, 0, 0]
+btn_width = 180
+btn_height = 120
+
 
 clock = pygame.time.Clock()
 fps = 60
@@ -17,24 +20,17 @@ screen = pygame.display.set_mode(size)
 
 def main():
     pygame.init()
-
-    main_screen()
-
+    myfont = pygame.font.SysFont("freesansbold", 30)
+    while True:
+        next = main_screen(myfont)
+        if(next == "bord"):
+            next = bord_screen(myfont)
+        elif(next == "tilbage"):
+            next = main_screen(myfont)
+        elif(next == False):
+            break
     pygame.quit()
     sys.exit
-
-
-def text_objects(text, font):
-    textSurface = font.render(text, True, pygame.Color(255, 255, 255))
-    return textSurface, textSurface.get_rect()
-
-
-def message_display(text, x, y):
-    largeText = pygame.font.Font('freesansbold.ttf', 20)
-    TextSurf, TextRect = text_objects(text, largeText)
-    TextRect.center = (x, y)
-    screen.blit(TextSurf, TextRect)
-    # pygame.display.update()
 
 
 def button(x, y, width, height, color):
@@ -44,34 +40,36 @@ def button(x, y, width, height, color):
     return button
 
 
-def main_screen():
-    # create and display buttons
-    tl_button = button(width/4*1-width/5/2, height/4,
-                       width/5, height/5, [255, 0, 0])
-    tm_button = button(width/4*2-width/5/2, height/4,
-                       width/5, height/5, [255, 0, 0])
-    tr_button = button(width/4*3-width/5/2, height/4,
-                       width/5, height/5, [255, 0, 0])
-    bl_button = button(width/4*1-width/5/2, height/2,
-                       width/5, height/5, [255, 0, 0])
-    bm_button = button(width/4*2-width/5/2, height/2,
-                       width/5, height/5, [255, 0, 0])
-    br_button = button(width/4*3-width/5/2, height/2,
-                       width/5, height/5, [255, 0, 0])
-
-    pygame.display.set_caption('ROPOX')
-
-    message_display("PROFIL", width/4*1, height/3+10)
-    message_display("BORD", width/4*2, height/3+10)
-    message_display("SKAB", width/4*3, height/3+10)
-    message_display("LÅS", width/4*1, height/1.75+15)
-    message_display("OVN", width/4*2, height/1.75+15)
-    message_display("INDSTILLINGER", width/4*3, height/1.75+15)
-
-    message_display("ROPOX", width/2, height/10)
-    pygame.display.update()
+def main_screen(myfont):
 
     while True:
+        # create and display buttons
+        tl_button = button(width/4*1-btn_width/2, height/4,
+                           btn_width, btn_height, [255, 0, 0])
+        tm_button = button(width/4*2-btn_width/2, height/4,
+                           btn_width, btn_height, [255, 0, 0])
+        tr_button = button(width/4*3-btn_width/2, height/4,
+                           btn_width, btn_height, [255, 0, 0])
+        bl_button = button(width/4*1-btn_width/2, height/2,
+                           btn_width, btn_height, [255, 0, 0])
+        bm_button = button(width/4*2-btn_width/2, height/2,
+                           btn_width, btn_height, [255, 0, 0])
+        br_button = button(width/4*3-btn_width/2, height/2,
+                           btn_width, btn_height, [255, 0, 0])
+
+        # display text for buttons
+        text("PROFIL", myfont, tl_button.center)
+        text("BORD", myfont, tm_button.center)
+        text("SKAB", myfont, tr_button.center)
+        text("LÅS", myfont, bl_button.center)
+        text("OVN", myfont, bm_button.center)
+        text("INDSTILLINGER", myfont, br_button.center)
+
+        # display headline
+        pygame.display.set_caption('ROPOX')
+        text("ROPOX", myfont, (width/2, height/10))
+
+        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return False
@@ -87,8 +85,7 @@ def main_screen():
                         'TopLeft button was pressed at {0}'.format(mouse_pos))
                 elif tm_button.collidepoint(mouse_pos):
                     print('TopMid button was pressed at {0}'.format(mouse_pos))
-                    bord_screen()
-                    break
+                    return "bord"
 
                 elif tr_button.collidepoint(mouse_pos):
                     print(
@@ -102,41 +99,42 @@ def main_screen():
                 elif br_button.collidepoint(mouse_pos):
                     print(
                         'BottomRight button was pressed at {0}'.format(mouse_pos))
-
         screen.fill(bg)
 
-        # pygame.display.update()
         clock.tick(fps)
 
 
-def bord_screen():
-    pygame.display.update()
-    print("bord screen")
-
-    tl_button = button(width/4*1-width/5/2, height/4,
-                       width/5, height/5, [255, 0, 0])
-    tm_button = button(width/4*2-width/5/2, height/4,
-                       width/5, height/5, [255, 0, 0])
-    tr_button = button(width/4*3-width/5/2, height/4,
-                       width/5, height/5, [255, 0, 0])
-    bl_button = button(width/4*1-width/5/2, height/2,
-                       width/5, height/5, [255, 0, 0])
-    bm_button = button(width/4*2-width/5/2, height/2,
-                       width/5, height/5, [255, 0, 0])
-    br_button = button(width/4*3-width/5/2, height/2,
-                       width/5, height/5, [255, 0, 0])
-
-    message_display("↑OP", width/4*1, height/3+10)
-    message_display("Højde", width/4*2, height/3+10)
-    message_display("Lås", width/4*3, height/3+10)
-    message_display("↓NED", width/4*1, height/1.75+15)
-    message_display("Profil", width/4*2, height/1.75+15)
-    message_display("Tilbage", width/4*3, height/1.75+15)
-
-    message_display("Bord", width/2, height/10)
-    pygame.display.update()
-
+def bord_screen(myfont):
+    h = 0
+    pygame.display.flip()
     while True:
+        # create and display buttons
+        tl_button = button(width/4*1-btn_width/2, height/4,
+                           btn_width, btn_height, [255, 0, 0])
+        tm_button = button(width/4*2-btn_width/2, height/4,
+                           btn_width, btn_height, [255, 0, 0])
+        tr_button = button(width/4*3-btn_width/2, height/4,
+                           btn_width, btn_height, [255, 0, 0])
+        bl_button = button(width/4*1-btn_width/2, height/2,
+                           btn_width, btn_height, [255, 0, 0])
+        bm_button = button(width/4*2-btn_width/2, height/2,
+                           btn_width, btn_height, [255, 0, 0])
+        br_button = button(width/4*3-btn_width/2, height/2,
+                           btn_width, btn_height, [255, 0, 0])
+        # display text for buttons
+        text("OP", myfont, tl_button.center)
+        text("HØJDE", myfont, tm_button.center)
+        text("LÅS", myfont, tr_button.center)
+        text("NED", myfont, bl_button.center)
+        text("PROFIL", myfont, bm_button.center)
+        text("TILBAGE", myfont, br_button.center)
+        text(str(h), myfont, (tm_button.center[0],tm_button.center[1]+30 ))
+
+        # display headline
+        pygame.display.set_caption('ROPOX')
+        text("ROPOX", myfont, (width/2, height/10))
+
+        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return False
@@ -150,7 +148,7 @@ def bord_screen():
                     # prints current location of mouse
                     print(
                         'TopLeft button was pressed at {0}'.format(mouse_pos))
-                    goUp();
+                    #rp.goUp()
                 elif tm_button.collidepoint(mouse_pos):
                     print('TopMid button was pressed at {0}'.format(mouse_pos))
                 elif tr_button.collidepoint(mouse_pos):
@@ -159,14 +157,25 @@ def bord_screen():
                 elif bl_button.collidepoint(mouse_pos):
                     print(
                         'BottomLeft button was pressed at {0}'.format(mouse_pos))
-                    goDown()
+                    #rp.goDown()
                 elif bm_button.collidepoint(mouse_pos):
                     print(
                         'BottomMid button was pressed at {0}'.format(mouse_pos))
                 elif br_button.collidepoint(mouse_pos):
                     print(
                         'BottomRight button was pressed at {0}'.format(mouse_pos))
+                    return "tilbage"
+        screen.fill(bg)
+
         clock.tick(fps)
+        h += 1
+
+
+def text(txt, myfont, location):
+    text_to_display = myfont.render(txt, 1, (255, 255, 255))
+    placement = (location[0]-text_to_display.get_rect().width/2,
+                 location[1]-text_to_display.get_rect().height/2)
+    screen.blit(text_to_display, placement)
 
 
 if __name__ == '__main__':
